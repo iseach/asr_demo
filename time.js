c8e117fdc7bfc8a98810e9f7efea8251511143ee
Zhanliang Liu <liang@zliu.org>
2022-10-14 18:12:15 +0800

    chota

--- time.js
+++ time.js
@@ -8,19 +8,19 @@ const time = {
       let h = Math.floor(this.second / (60 * 60)).toString().padStart(2, '0');
       let m = (Math.floor(this.second / 60) - 60 * h).toString().padStart(2, '0');
       let s = (this.second % 60).toString().padStart(2, '0');
-      document.querySelector('h1').innerHTML = `${h}:${m}:${s}`;
+      document.querySelector('#timer').innerHTML = `${h}:${m}:${s}`;
     }, 1000);
   },
 
   stop() {
     clearInterval(this.timer);
     this.second = 0;
-    document.querySelector('h1').innerHTML = `00:00:00`;
+    document.querySelector('#timer').innerHTML = `00:00:00`;
     this.timer = null;
   },
 
   init() {
     this.second = 0;
-    document.querySelector('h1').innerHTML = `00:00:00`;
+    document.querySelector('#timer').innerHTML = `00:00:00`;
   }
 };
