59568445e9e033a3a389bb123ed9dd1a148839e8
Zhanliang Liu <liang@zliu.org>
2022-10-14 13:17:28 +0800

    add wave animation effect

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var logBox = document.getElementById('log-box');
 
 const btn = document.querySelector("#btn");
+const wave = document.querySelector("#wave");
 
 var ws = null;
 var record = null;
@@ -14,26 +15,26 @@ var Recorder = function (stream) {
   var audioInput = context.createMediaStreamSource(stream);
   var recorder = context.createScriptProcessor(4096, 1, 1);
   var audioData = {
-      size: 0,
-      buffer: [],
-      inputSampleRate: 48000,
-      inputSampleBits: 16,
-      outputSampleRate: sampleRate,
-      oututSampleBits: sampleBits,
-      clear: function () {
-        this.buffer = [];
-        this.size = 0;
-      },
-      input: function (data) {
-        this.buffer.push(new Float32Array(data));
-        this.size += data.length;
-      },
-      compress: function () {
+    size: 0,
+    buffer: [],
+    inputSampleRate: 48000,
+    inputSampleBits: 16,
+    outputSampleRate: sampleRate,
+    oututSampleBits: sampleBits,
+    clear: function () {
+      this.buffer = [];
+      this.size = 0;
+    },
+    input: function (data) {
+      this.buffer.push(new Float32Array(data));
+      this.size += data.length;
+    },
+    compress: function () {
       var data = new Float32Array(this.size);
       var offset = 0;
       for (var i = 0; i < this.buffer.length; i++) {
-      data.set(this.buffer[i], offset);
-      offset += this.buffer[i].length;
+        data.set(this.buffer[i], offset);
+        offset += this.buffer[i].length;
       }
       var compression = parseInt(this.inputSampleRate / this.outputSampleRate);
       var length = data.length / compression;
@@ -41,9 +42,9 @@ var Recorder = function (stream) {
       var index = 0,
       j = 0;
       while (index < length) {
-      result[index] = data[j];
-      j += compression;
-      index++;
+        result[index] = data[j];
+        j += compression;
+        index++;
       }
       return result;
     },
@@ -56,8 +57,8 @@ var Recorder = function (stream) {
       var data = new DataView(buffer);
       var offset = 0;
       for (var i = 0; i < bytes.length; i++, offset += 2) {
-      var s = Math.max(-1, Math.min(1, bytes[i]));
-      data.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+        var s = Math.max(-1, Math.min(1, bytes[i]));
+        data.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
       }
       return new Blob([data]);
     }
@@ -73,23 +74,22 @@ var Recorder = function (stream) {
       var tmparr = new Int8Array(1024);
       var j = 0;
       for (var i = 0; i < arr.byteLength; i++) {
-          tmparr[j++] = arr[i];
-          if (((i + 1) % 1024) == 0) {
-          ws.send(tmparr);
-          if (arr.byteLength - i - 1 >= 1024) {
-            tmparr = new Int8Array(1024);
-          } else {
-            tmparr = new Int8Array(arr.byteLength - i - 1);
-          }
-          j = 0;
-          }
-          if ((i + 1 == arr.byteLength) && ((i + 1) % 1024) != 0) {
-          ws.send(tmparr);
-          }
+        tmparr[j++] = arr[i];
+        if (((i + 1) % 1024) == 0) {
+        ws.send(tmparr);
+        if (arr.byteLength - i - 1 >= 1024) {
+          tmparr = new Int8Array(1024);
+        } else {
+          tmparr = new Int8Array(arr.byteLength - i - 1);
+        }
+        j = 0;
       }
-
+      if ((i + 1 == arr.byteLength) && ((i + 1) % 1024) != 0) {
+        ws.send(tmparr);
       }
-    };
+    }
+  }
+};
     reader.readAsArrayBuffer(audioData.encodePCM());
     audioData.clear();
   };
@@ -101,22 +101,22 @@ var Recorder = function (stream) {
   }
 
   this.stop = function () {
-      time.stop();
-      recorder.disconnect();
+    time.stop();
+    recorder.disconnect();
   }
 
   this.getBlob = function () {
-      return audioData.encodePCM();
+    return audioData.encodePCM();
   }
 
   this.clear = function () {
-      audioData.clear();
+    audioData.clear();
   }
 
   recorder.onaudioprocess = function (e) {
-      var inputBuffer = e.inputBuffer.getChannelData(0);
-      audioData.input(inputBuffer);
-      sendData();
+    var inputBuffer = e.inputBuffer.getChannelData(0);
+    audioData.input(inputBuffer);
+    sendData();
   }
 }
 
@@ -124,49 +124,48 @@ function useWebSocket() {
   ws = new WebSocket("wss://asr.nlp.ac.cn/asr");
   ws.binaryType = 'arraybuffer';
   ws.onopen = function () {
-      let newNode = document.createElement('div');
-      newNode.innerHTML = 'Websocket Opened.';
+    let newNode = document.createElement('div');
+    newNode.innerHTML = 'Websocket Opened.';
+    logBox.appendChild(newNode);
+
+    if (ws.readyState == 1) {
+      newNode = document.createElement('div');
+      newNode.innerHTML = 'Websocket Connected.';
       logBox.appendChild(newNode);
 
-      if (ws.readyState == 1) {
-        newNode = document.createElement('div');
-        newNode.innerHTML = 'Websocket Connected.';
-        logBox.appendChild(newNode);
+      record.start();
 
-        record.start();
-
-        newNode = document.createElement('div');
-        newNode.innerHTML = 'Sending data via websocket…';
-        logBox.appendChild(newNode);
-
-      }
+      newNode = document.createElement('div');
+      newNode.innerHTML = 'Sending data via websocket…';
+      logBox.appendChild(newNode);
+    }
   };
 
   ws.onmessage = function (msg) {
-      console.info(msg)
-      let newNode = document.createElement('div');
-      newNode.innerHTML = '<span style="color:red">Websocket Response: </span>';
-      logBox.appendChild(newNode);
-      newNode = document.createElement('div');
-      newNode.innerHTML = '<span style="color:red">' + msg.data + '</span>';
-      logBox.appendChild(newNode);
+    console.info(msg)
+    let newNode = document.createElement('div');
+    newNode.innerHTML = '<span style="color:red">Websocket Response: </span>';
+    logBox.appendChild(newNode);
+    newNode = document.createElement('div');
+    newNode.innerHTML = '<span style="color:red">' + msg.data + '</span>';
+    logBox.appendChild(newNode);
   }
 
   ws.onerror = function (err) {
-      console.info(err)
-      let newNode = document.createElement('div');
-      newNode.innerHTML = '<span style="color:red">Websocket Error.</span>';
-      logBox.appendChild(newNode);
+    console.info(err)
+    let newNode = document.createElement('div');
+    newNode.innerHTML = '<span style="color:red">Websocket Error.</span>';
+    logBox.appendChild(newNode);
   }
 
   ws.onclose = function (msg) {
-      console.info(msg)
-      record.stop();
-      record = null;
+    console.info(msg)
+    record.stop();
+    record = null;
 
-      let newNode = document.createElement('div');
-      newNode.innerHTML = 'WebSocket Closed.';
-      logBox.appendChild(newNode);
+    let newNode = document.createElement('div');
+    newNode.innerHTML = 'WebSocket Closed.';
+    logBox.appendChild(newNode);
   }
 }
 
@@ -210,8 +209,10 @@ btn.addEventListener("click", ()=>{
   if (btn.getAttribute("src") == "start.svg") {
     start();
     btn.setAttribute("src", "stop.svg");
+    wave.style.display = "block";
   } else {
     stop();
     btn.setAttribute("src", "start.svg");
+    wave.style.display = "none";
   }
 });
